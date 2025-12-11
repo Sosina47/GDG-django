db8a3352e662e029f0a7cef516cda3ce7548bbc7
sosina47 <sosinagirmay15@gmail.com>
2025-12-11 21:29:19 +0300

    Remove duplicate week_2_tasks from root

--- week_2_tasks/1.py
+++
@@ -1,5 +0,0 @@
-items = int(input('how many items do you wanna buy? '))
-if items > 3:
-    print('Discount Applied!')
-else:
-    print('No Discount!')
--- week_2_tasks/4.py
+++
@@ -1,8 +0,0 @@
-def get_grade(student_grades, student_name):
-    try:
-        return student_grades[student_name]
-    except:
-        return 'student not found in the system'
-
-student_grades = {'sosina': 'C', 'sara': 'B', 'Anket': 'A'}
-print(get_grade(student_grades, 'sosina'))
