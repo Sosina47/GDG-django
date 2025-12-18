3989b17819c17e09132562198937724a8614d287
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2025-12-18 23:24:16 +0300

    Delete GDG_django/week_1_tasks directory

--- GDG_django/week_1_tasks/GDG_django/wee_3_tasks/2.py
+++
@@ -1,6 +0,0 @@
-class Dog():
-    def bark(self):
-        print('woof!')
-
-d1 = Dog()
-d1.bark()
--- GDG_django/week_1_tasks/GDG_django/week_2_tasks/10.py
+++
@@ -1,8 +0,0 @@
-x = int(input('enter a number: '))
-rev = 0
-temp = x
-while temp > 0:
-    rev *= 10
-    rev += temp % 10
-    temp //= 10
-print(rev == x)
